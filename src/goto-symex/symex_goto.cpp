bae4901128156629b13b2eea7dde3071952cf329
Yosephine Setiawan <agatayosephine@gmail.com>
2025-02-07 17:50:39 +0100

    Slowly cleaning up the code

--- src/goto-symex/symex_goto.cpp
+++ src/goto-symex/symex_goto.cpp
@@ -593,9 +593,6 @@ void goto_symext::retrace(std::list<int> trace, bool firstCall) {
   std::queue<std::reference_wrapper<goto_symex_statet>> newStack;
 
   while (!trace_stack.empty()) {
-    // Debug logging
-    std::cout << "Processing trace_stack element" << std::endl;
-
     if (firstCall && pointer->trace != trace) {
       newStack.push(*pointer);
     }
