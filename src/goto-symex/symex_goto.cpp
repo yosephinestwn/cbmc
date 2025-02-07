5d3f1e4e50d85cfe26efe0ba21667fd31d76901f
Yosephine Setiawan <agatayosephine@gmail.com>
2025-02-07 17:24:36 +0100

    Cleaning the code slowly

--- src/goto-symex/symex_goto.cpp
+++ src/goto-symex/symex_goto.cpp
@@ -624,16 +624,14 @@ void goto_symext::retrace(std::list<int> trace, bool firstCall) {
   // Replace the old stack with the new stack
   trace_stack = newStack;
 
-  std::cout << "trace_stack looped" << std::endl;
-
   if (nodes.empty() || nodes.size() < 1) {
-    std::cout << "There is no path with such traces" << std::endl;
+    std::cout << "\nThere is no path with such traces" << std::endl;
     return;
   }
 
   // Base case for recursion
   if (trace.size() <= 1) {
-    std::cout << "The path that the program takes:" << std::endl;
+    std::cout << "\nThe path that the program takes:" << std::endl;
     for (auto& element : nodes) {
       std::cout << element.get().source.pc->source_location() << std::endl;
     }
