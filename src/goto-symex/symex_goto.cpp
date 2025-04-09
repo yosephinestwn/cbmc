73d41489f1b37ad2684c621dfde7b11cd658b0ed
Yosephine Setiawan <agatayosephine@gmail.com>
2025-04-09 14:28:00 +0200

    Adding debugging messages

--- src/goto-symex/symex_goto.cpp
+++ src/goto-symex/symex_goto.cpp
@@ -260,6 +260,18 @@ void goto_symext::symex_goto_retrace(statet &state, std::vector<int> trace)
 
   const bool backward = instruction.is_backwards_goto();
 
+  printf("Current conditional branching: %d\n", trace[trace_index]);
+
+  printf("Current instruction: %s\n", instruction.source_location().as_string().c_str());
+
+  printf("Next Goto of this instruction: %s\n", goto_target->source_location().as_string().c_str());
+
+  goto_programt::const_targett next_instruction = state.source.pc;
+
+  next_instruction++;
+
+  printf("Next instruction: %s\n", next_instruction->source_location().as_string().c_str());
+
   symex_targett::sourcet original_source=state.source;
   goto_programt::const_targett new_state_pc;
 
